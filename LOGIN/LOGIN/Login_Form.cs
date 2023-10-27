e1dc82d4c2555fe57fb0f48c46cee0225ab5a7f1
krishnamoorthy81241 <krishnamoorthy81241@gmail.com>
2023-10-27 13:31:11 +0530

    Add project files.

---
+++ LOGIN/LOGIN/Login_Form.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace LOGIN
+{
+    public partial class Login_Form : Form
+    {
+        public Login_Form()
+        {
+            InitializeComponent();
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SqlConnection sqlcon = new SqlConnection(@"Data Source = KRISHNA;Initial Catalog = LOGIN;Integrated Security = True");
+            string query = "Select * from tbl_Login Where username = '"+txtUserName.Text.Trim()+"' and password = '"+txtPassword.Text.Trim()+"'";
+            SqlDataAdapter sda = new SqlDataAdapter(query, sqlcon);
+            DataTable dt = new DataTable();
+            sda.Fill(dt);
+            if (dt.Rows.Count == 1 )
+            {
+                Main_Form main = new Main_Form();
+                this.Hide();
+                main.Show();
+            }
+            else
+            {
+                MessageBox.Show("Check Your UserName and Password");
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
